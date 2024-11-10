ccbc8b7221c6bfbcb9ce1e38637eaea3683cf502
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2024-11-09 22:54:35 -0800

    convert-string-to-array.js

---
+++ convert-string-to-array.js
@@ -0,0 +1,16 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input ==> Output):
+// "Robin Singh" ==> ["Robin", "Singh"]
+
+// MY SOLUTION STARTS HERE 
+
+
+function stringToArray(string){
+
+let newSplit = string.split(" ")
+return newSplit 
+
+}
+
+console.log(stringToArray("Hello")) 
