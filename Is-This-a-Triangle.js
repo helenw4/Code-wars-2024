300c5801b2c4b90e595291f9974ebb466454713e
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2024-11-30 18:58:41 -0800

    Is-This-a-Triangle.js

---
+++ Is-This-a-Triangle.js
@@ -0,0 +1,24 @@
+
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+// (In this case, all triangles must have surface greater than 0 to be accepted).
+
+// Examples:
+
+// Input -> Output
+// 1,2,2 -> true
+
+
+
+
+// SOLUTION STARTS HERE 
+
+function isTriangle(a, b, c) {
+  // Check for positive side lengths and the triangle inequality
+  if (a > 0 && b > 0 && c > 0 && (a + b > c) && (a + c > b) && (b + c > a)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isTriangle(3,3,3))    
