ad1be22e8a4e0b625a325d5d340edbd407a3a138
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-01-26 19:14:31 -0800

    Rename Is-This-a-Triangle.js to Is-This-a-Triangle?.js

--- Is-This-a-Triangle?.js
+++ Is-This-a-Triangle?.js
@@ -1,14 +1,12 @@
 
-// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+// Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of a given length and false in any other case.
 
-// (In this case, all triangles must have surface greater than 0 to be accepted).
+// (In this case, all triangles must have a surface greater than 0 to be accepted).
 
 // Examples:
 
 // Input -> Output
 // 1,2,2 -> true
-
-
 
 
 // SOLUTION STARTS HERE 
