4aed4f77f2c0a0cbea8b072afac573ce13fbf216
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-02-08 14:28:23 -0800

    Get-the-mean-of-an-array.js

--- Get-the-mean-of-an-array.js
+++ Get-the-mean-of-an-array.js
@@ -1,11 +1,11 @@
-// It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+// It's the academic year's end, the fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
 
 // Return the average of the given array rounded down to its nearest integer.
 
 // The array will never be empty.
 
 
-// MY SOLUTION 
+// MY SOLUTION: 
 
 function getAverage(marks){
 let averMarks = marks.reduce((acc,curVal) => acc + curVal, 0   );
