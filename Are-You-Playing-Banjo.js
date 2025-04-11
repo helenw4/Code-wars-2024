94ab9aa732a33737487961a279e257829cee85a0
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-04-11 15:30:55 -0700

    Are-You-Playing-Banjo.js

--- Are-You-Playing-Banjo.js
+++ Are-You-Playing-Banjo.js
@@ -1,7 +1,7 @@
 // Create a function that answers the question "Are you playing banjo?".
-// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+// If your name starts with the letter "R" or lowercase "r", you are playing banjo!
 
-// The function takes a name as its only argument, and returns one of the following strings:
+// The function takes a name as its only argument and returns one of the following strings:
 
 // name + " plays banjo" 
 // name + " does not play banjo"
