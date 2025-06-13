337f9f808092fb036e5d8d21b6104f51c9e885c1
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-06-12 18:22:32 -0700

    Return-negative.js

--- Return-negative.js
+++ Return-negative.js
@@ -1,10 +1,10 @@
-// In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+// In this simple assignment, you are given a number and have to make it negative. But maybe the number is already negative?
 
 // Examples
 // makeNegative(1);    // return -1
 
 
-// MY SOLUTION: 
+//  MY SOLUTION: 
 
 function makeNegative(num) {
   if (num > 0){
