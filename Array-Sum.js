c940cb8a6ebf49b2552c3a81237b36b292cab16c
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2025-10-24 22:04:03 -0700

    Array-Sum.js

--- Array-Sum.js
+++ Array-Sum.js
@@ -1,6 +1,6 @@
-//  Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+//   Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 
-// MY SOLUTION STARTS: 
+//   MY SOLUTION STARTS: 
 
 function sum (numbers) {
   let arrSum = numbers.reduce((acc, curVal) => acc + curVal, 0)
