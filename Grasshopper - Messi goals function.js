31b97f8876ace31bf8cd4774ec218d3a05aef340
Helen_Abera <139908625+helenw4@users.noreply.github.com>
2026-05-13 14:47:29 -0700

    Grasshopper - Messi goals function.js

--- Grasshopper - Messi goals function.js
+++ Grasshopper - Messi goals function.js
@@ -13,6 +13,7 @@
 
 // SOLUTION STARTS HERE: 
 
+
 function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
   let messiGoals = laLigaGoals + copaDelReyGoals + championsLeagueGoals; 
   return messiGoals; 
